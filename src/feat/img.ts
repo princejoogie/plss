6dce4ec6c8e7eb517f135ed44f12faaf4987db42
princejoogie <princejoogie@gmail.com>
2022-11-04 12:37:49 +0800

    fix: use node-fetch
    modified:   package.json
    modified:   src/feat/img.ts
    modified:   yarn.lock

--- src/feat/img.ts
+++ src/feat/img.ts
@@ -1,9 +1,10 @@
 import fs from "fs";
-import axios from "axios";
+import fetch from "node-fetch";
 
 import { openai } from "../config";
 
 export const img = async (query: string) => {
+  console.log("Generating image...");
   const response = await openai.createImage({
     prompt: query,
     n: 1,
@@ -23,12 +24,13 @@ export const img = async (query: string) => {
   const url = response.data.data[0].url.trim();
   const name = `${new Date().toISOString()}.png`;
 
+  console.log("Downloading image...");
   await downloadImage(url, name);
 
   console.log(`Image saved to ${name}`);
 };
 
 const downloadImage = async (url: string, name: string) => {
-  const response = await axios.get(url, { responseType: "stream" });
-  response.data.pipe(fs.createWriteStream(name));
+  const res = await fetch(url);
+  res.body?.pipe(fs.createWriteStream(name));
 };
